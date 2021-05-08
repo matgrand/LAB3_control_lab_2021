30f074d6a27393d713b7367bc377ee807ba6fb11
grandin <matteo.grandin@gmail.com>
2021-05-08 23:41:47 +0200

    working up to 2.4 8

--- LAB3_Matteo/ass2_4_1_4_LQR.m
+++ LAB3_Matteo/ass2_4_1_4_LQR.m
@@ -54,32 +54,17 @@ plot_scope(thh_scope, "Nominal LQR Regulator Root Locus");
 % 3 more general cost function
 step_height = 50; %deg
 
-rho = 100; %balance between input and state (rho small -> input very costly)
-dev_thh = 0.25*0.3*step_height*(pi/180); %maximum variation of the hub
-dev_thd = 1*(pi/36); %maximum variation of the angle of the beam wrt the hub
+rho = 1.2; %balance between input and state (rho big -> input very costly)
+dev_thh = 0.7*0.3*step_height*(pi/180); %maximum variation of the hub
+dev_thd = (pi/36); %maximum variation of the angle of the beam wrt the hub
 
 dev_u = 10; %input maximum variation (to avoid saturation)
-
-%since our current ss uses thb and thh we need to switch to thd (relative position of the beam)
-
-T = [1 0 0 0; 1 1 0 0; 0 0 1 0; 0 0 1 1];
-
-A_1 = T\A*T;
-B_1 = T\B;
-C_1 = C*T;
-D_1 = D;
-
-sysG_1 = ss(A_1, B_1, C_1, D_1);
 
 %using Bryson's rule
 Q = diag([1/dev_thh^2 1/dev_thd^2 0 0]);
 R = rho/dev_u^2;
 
-K_ss_1 = lqr(sysG_1, Q, R);
-K_ss = K_ss_1*T; %switch back to original system
-
-%maybe it's correct this way, but it doesn't make sense
-%K_ss = lqr(sysG, Q, R);
+K_ss = lqr(sysG, Q, R);
 
 %4 validation 
 %NOMINAL LQR 
---
+++ LAB3_Matteo/ass2_4_9_LQR.m
@@ -0,0 +1,5 @@
+%ass 2.4 (9)
+
+% FREQUENCY DEPENDENT WEIGHT
+
+%resonant frequency of the 
