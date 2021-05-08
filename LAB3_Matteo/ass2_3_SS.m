011d8584687d864dd7fb900b2bcf13c2395e3da6
grandin <matteo.grandin@gmail.com>
2021-05-08 19:36:32 +0200

    minor modifications

--- LAB3_Matteo/ass2_3_SS.m
+++ LAB3_Matteo/ass2_3_SS.m
@@ -4,47 +4,10 @@
 close all;
 clear all;
 
-load_param_resonance_load;
-important_parameters;
-
 ts5 = 0.85; %settling time 
 Mp = 0.30; %overshoot 
 
-%STATE SPACE
-s = tf('s');
-
-%real derivative transfer function for finding and filtering w
-w_c = 2*pi*50;
-delta_f = 1/sqrt(2);
-H_w = (w_c^2*s)/(s^2+2*delta_f*w_c*s+w_c^2); %real derivative
-[numH_w,denH_w] = tfdata(H_w, 'v'); %to be used it in a simulink block
-
-%matrix A 
-a31 = -k/(N*N*Jeq);
-a32 = -a31;
-a41 = k/Jb;
-a42 = -a41;
-a33 = -(1/Jeq)*(Beq+(Kt*Ke)/Req);
-a44 = -Bb/Jb;
-A = [0 0 1 0; 
-    0 0 0 1;
-    a31 a32 a33 0;
-    a41 a42 0 a44];
-
-%B
-b3 = (Kt*Kdrv)/(N*Jeq*Req);
-B = [0;0; b3;0];
-
-C = [1 0 0 0];
-D = [0];
-
-motor_sys = ss(A,B,C,D);
-
-
-delta = (log(1/Mp))/(sqrt(pi^2+(log(1/Mp))^2));
-wn = 3/(delta*ts5);
-phi = atan(sqrt(1-delta^2)/delta);
-
+state_space_design_LQR;
 
 %NOMINAL CONTROL
 p1 = -wn*exp(+1i*phi);
@@ -52,15 +15,10 @@ p2 = -wn*exp(-1i*phi);
 p3 = -wn*exp(+1i*phi/2);
 p4 = -wn*exp(-1i*phi/2);
 
-poles = 2*[p1 p2 p3 p4];
+poles = 3*[p1 p2 p3 p4];
 
 K_ss = real(place(A,B, poles)) 
 K_ih = 0; %no integral action (but needed for the model)
-
-F = [A B; C D];
-Nxu = F\[0; 0; 0; 0; 1];
-Nx = [Nxu(1); Nxu(2); Nxu(3); Nxu(4)]
-Nu = Nxu(5)
 
 %simulink model
 
@@ -84,7 +42,7 @@ Be = [0; B];
 
 p5 = -wn;
 
-poles = 2*[p1 p2 p3 p4 p5];
+poles = 3*[p1 p2 p3 p4 p5];
 
 Ke = acker(Ae, Be, poles);
 K_ih = Ke(1)
@@ -97,6 +55,27 @@ enable_integral_action = 1;
 sim(simulink_system);
 
 plot_scope(thh_scope, "Robust SS regulator with integral action");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- LAB3_Matteo/ass2_4_1_4_LQR.m
+++ LAB3_Matteo/ass2_4_1_4_LQR.m
@@ -4,54 +4,13 @@
 clear all;
 close all;
 
-load_param_resonance_load;
-important_parameters;
-
 ts5 = 0.85; %settling time 
 Mp = 0.30; %overshoot 
 
-%STATE SPACE
-s = tf('s');
-
-%real derivative transfer function for finding and filtering w
-w_c = 2*pi*50;
-delta_f = 1/sqrt(2);
-H_w = (w_c^2*s)/(s^2+2*delta_f*w_c*s+w_c^2); %real derivative
-[numH_w,denH_w] = tfdata(H_w, 'v'); %to be used it in a simulink block
-
-%for findiing poles (maybe useless)
-delta = (log(1/Mp))/(sqrt(pi^2+(log(1/Mp))^2));
-wn = 3/(delta*ts5);
-phi = atan(sqrt(1-delta^2)/delta);
-
-%state space design
-%matrix A 
-a31 = -k/(N*N*Jeq);
-a32 = -a31;
-a33 = -(1/Jeq)*(Beq+(Kt*Ke)/Req);
-a41 = k/Jb;
-a42 = -a41;
-a44 = -Bb/Jb;
-A = [0 0 1 0; 
-    0 0 0 1;
-    a31 a32 a33 0;
-    a41 a42 0 a44];
-
-%B
-b3 = (Kt*Kdrv)/(N*Jeq*Req);
-B = [0;0; b3;0];
-
-C = [1 0 0 0];
-D = [0];
+state_space_design_LQR;
 
 sysG = ss(A,B,C,D); %state space
 sysGp = ss(-A, -B, C, D); %inverse state space
-
-%Nx Nu
-F = [A B; C D];
-Nxu = F\[0; 0; 0; 0; 1];
-Nx = [Nxu(1); Nxu(2); Nxu(3); Nxu(4)]
-Nu = Nxu(5)
 
 %root locus analysis
 r = 1/3850; %tuned value
@@ -89,18 +48,53 @@ enable_integral_action = 0; %disable integral action
 step_height = 50; %deg
 
 sim(simulink_system);
-plot_scope(thh_scope, "Nominal LQR Regulator");
+plot_scope(thh_scope, "Nominal LQR Regulator Root Locus");
 
 
 % 3 more general cost function
 step_height = 50; %deg
 
-dev_thh = 0.3*step_height*(pi/180); %maximum variation of the hub
-dev_thd = pi/36; %maximum variation of the angle of the beam wrt the hub
+rho = 100; %balance between input and state (rho small -> input very costly)
+dev_thh = 0.25*0.3*step_height*(pi/180); %maximum variation of the hub
+dev_thd = 1*(pi/36); %maximum variation of the angle of the beam wrt the hub
 
 dev_u = 10; %input maximum variation (to avoid saturation)
 
-%Q = diag([])
+%since our current ss uses thb and thh we need to switch to thd (relative position of the beam)
+
+T = [1 0 0 0; 1 1 0 0; 0 0 1 0; 0 0 1 1];
+
+A_1 = T\A*T;
+B_1 = T\B;
+C_1 = C*T;
+D_1 = D;
+
+sysG_1 = ss(A_1, B_1, C_1, D_1);
+
+%using Bryson's rule
+Q = diag([1/dev_thh^2 1/dev_thd^2 0 0]);
+R = rho/dev_u^2;
+
+K_ss_1 = lqr(sysG_1, Q, R);
+K_ss = K_ss_1*T; %switch back to original system
+
+%maybe it's correct this way, but it doesn't make sense
+%K_ss = lqr(sysG, Q, R);
+
+%4 validation 
+%NOMINAL LQR 
+enable_integral_action = 0; %disable integral action
+step_height = 50; %deg
+
+sim(simulink_system);
+plot_scope(thh_scope, "Nominal LQR Regulator Custom Design 1");
+
+
+
+
+
+
+
 
 
 
